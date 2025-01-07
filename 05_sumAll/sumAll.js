57a9b4fbda1953f14b8426a865ce5b52feb6625a
onturaa <174931989+onturaa@users.noreply.github.com>
2025-01-07 15:42:12 -0500

    Complete 05-07 According to Spec
    Implement functions according to the specification.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
 
+    let sum = 0;
+    
+    if (start < 0 || end < 0 || Number.isInteger(start) === false || Number.isInteger(end) === false) {
+        return 'ERROR';
+    }
+    if (start === end) {
+        return start;
+    }
+    else if (start > end) {
+        let temp = start;
+        start = end;
+        end = temp;
+    }
+
+    for (let i = start; i < end + 1; i++) {
+        sum = sum + i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,21 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitTemp) {
+  let newTemp = (fahrenheitTemp - 32) * 5/9;
+  if (((fahrenheitTemp - 32) * 5) % 9 === 0) {
+    return newTemp
+  }
+  else {
+    return Number(newTemp.toFixed(1));
+  }
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusTemp) {
+  let newTemp = (celsiusTemp * 9/5) + 32;
+  if ((celsiusTemp *9) % 5 === 0) {
+    return newTemp;
+  }
+  else {
+    return Number(newTemp.toFixed(1));
+  }
 };
 
 // Do not edit below this line
