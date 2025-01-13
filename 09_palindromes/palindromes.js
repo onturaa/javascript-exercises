d1d0e7a0885b9a4e89b7cce1a82124fb8e3aff91
onturaa <174931989+onturaa@users.noreply.github.com>
2025-01-12 20:19:40 -0500

    Implement a Palindrome-Finding Algorithm
    Make sure strings ignore spaces and punctuation,
    can work with upper-case letters, and can work with
    one or more words.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,30 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    let array = string.split('');
+
+    let newStr = '';
+    array.forEach(elem => {
+        if (elem !== '!' && elem !== '.' && elem !== ',' && elem !== ':' && elem !== ';') {
+            newStr = newStr + elem;
+        }
+    });
+
+    newStr = newStr.split(' ').join('');
+
+    let left = (newStr.slice(0, newStr.length / 2)).toLowerCase();
+    let right = '';
+
+    if (newStr.length % 2 === 0) {
+        right = (newStr.slice(newStr.length / 2)).toLowerCase();
+    }
+    else
+    {
+        right = (newStr.slice((newStr.length / 2) + 1)).toLowerCase();
+    }
+
+    right = right.split('').reverse().join('');
+
+    return (left === right) ? true : false;
 };
 
 // Do not edit below this line
