4d2839ef68dd61a81ab32f7926edc321599af3e2
onturaa <174931989+onturaa@users.noreply.github.com>
2025-01-12 23:41:11 -0500

    Implement Algorithm to Find Oldest Person
    Use array methods to sort assign ages to people, and
    then to sort through those ages, returning the
    person with the oldest age.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const findTheOldest = function(people) {
+    people.map(person => {
+        if (typeof person.yearOfDeath === 'undefined') {
+            person['age'] = new Date().getFullYear() - person.yearOfBirth;
+        }
+        else {
+            person['age'] = person.yearOfDeath - person.yearOfBirth;
+        }
+    });
 
+    const oldestArr = people.sort((personA, personB) => personA.age > personB.age ? -1 : 1);
+
+    return oldestArr[0];
 };
 
 // Do not edit below this line
