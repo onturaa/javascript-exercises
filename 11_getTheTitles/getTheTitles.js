dcb16c16c8a5071377a44678400fd9d31d456d9a
onturaa <174931989+onturaa@users.noreply.github.com>
2025-01-12 20:54:42 -0500

    Implement Function to Retrieve Book Object Title
    Create new array of book titles by using .map() to
    access each book object's title property.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,11 @@
-const getTheTitles = function() {
+const getTheTitles = function(booksArr) {
+    let titleArr = [];
 
+    booksArr.map(book => {
+        titleArr.push(book.title);
+    });
+
+    return titleArr;
 };
 
 // Do not edit below this line
