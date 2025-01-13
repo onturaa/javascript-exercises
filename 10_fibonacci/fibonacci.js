d994933500eb70e0f7d88a038df76887a3e39279
onturaa <174931989+onturaa@users.noreply.github.com>
2025-01-12 20:45:33 -0500

    Implement Fibonacci Function
    Implement algorithm to add the previous two indices
    in array together to create new index. Then, push
    new index onto array. Make sure algorithm works with
    strings.

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,25 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+    let array = [1, 1];
+    let originalArr = [...array];
+    let num1 = 0;
+    let num2 = 0;
 
+    number = parseInt(number);
+
+    if (number === 0) {
+        return 0;
+    } else if (number < 0) {
+        return 'OOPS';
+    }
+
+    for (let i = 0; i < number - 2; i++) {
+        num2 = array.pop();
+        num1 = array.pop();
+        array.push(num2, num1 + num2);
+        originalArr.push(num1 + num2);
+    }
+
+    return originalArr.pop();
 };
 
 // Do not edit below this line
